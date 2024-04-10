7b2886ea5815556ea34a0a5ee80b2538afbc4c5e
Pankaj1547 <104687604+Pankaj1547@users.noreply.github.com>
2024-04-10 23:00:10 +0530

    Update 20240405162104-file-activity.js
    file upload wokring

--- Server/migrations/20240405162104-file-activity.js
+++ Server/migrations/20240405162104-file-activity.js
@@ -1,34 +1,40 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-     await queryInterface.createTable('FileActivity', {
-        id: {
-          allowNull: false,
-          autoIncrement: true,
-          primaryKey: true,
-          type: Sequelize.INTEGER
-        },
-        userId: {
-          type: Sequelize.INTEGER
-        },
-        uploadTime: {
-          type: Sequelize.DATE
-        },
-        downloadTime: {
-          type: Sequelize.DATE
-        },
-        filename: {
-          type: Sequelize.STRING
-        },
-        IPAddress: {
-          type: Sequelize.STRING
-      }
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("fileActivities", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+      },
+      uploadTime: {
+        type: Sequelize.DATE,
+      },
+      downloadTime: {
+        type: Sequelize.DATE,
+      },
+      filename: {
+        type: Sequelize.STRING,
+      },
+      IPAddress: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+      },
     });
   },
 
-  async down (queryInterface, Sequelize) {
-     await queryInterface.dropTable('FileActivity'); 
-  }
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("fileActivities");
+  },
 };
